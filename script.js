fdbc9bf022b0685624c552da1741c2125158c9bd
directx3r <prabanath2003@gmail.com>
2024-05-27 17:31:54 +0800

    authorizationEndpoint
    added authorizationEndpoint

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const clientId = 'Ov23cttySQMaE3IwT55f';
 const redirectUri = 'https://directx3r.github.io/diy_self_service/callback.html';
+const authorizationEndpoint = 'https://github.com/login/oauth/authorize';
 const scope = 'repo'; // Adjust the scope based on your requirements
 
 document.getElementById('loginButton').addEventListener('click', () => {
