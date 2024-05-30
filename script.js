90ba58aa34d836075fd226f4381825332674dd6d
directx3r <prabanath2003@gmail.com>
2024-05-30 13:56:20 +0800

    changes to client ID
    changes to client ID

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const clientId = 'Iv23lv23li330STWqvRxJbYs';
+const clientId = 'Ov23cttySQMaE3IwT55f';
 const redirectUri = 'https://directx3r.github.io/diy_self_service/callback.html';
 const authorizationEndpoint = 'https://github.com/login/oauth/authorize';
 const tokenEndpoint = 'https://github.com/login/oauth/access_token';
